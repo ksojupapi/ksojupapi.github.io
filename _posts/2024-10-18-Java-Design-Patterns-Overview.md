db2c65623c3a85b2f404e25c4ea1870163bbb3b5
Your Name <yourName@student.unsw.edu.au>
2024-10-18 16:49:49 +1100

    created the design patterns post

---
+++ _posts/2024-10-18-Java-Design-Patterns-Overview.md
@@ -0,0 +1,25 @@
+---
+layout: post
+title: "Java Design Patterns Overview"
+date: 2024-10-18 16:45:17 +1100
+categories: [category1, category2]
+tags: [tag1, tag2]
+toc: true
+toc_label: "Table of Contents"
+toc_icon: "list"
+author: "Your Name"
+---
+
+## Introduction
+
+**Design patterns** are the template on a structure level.
+These templates are focused on managing the common design problems with the object-oriented perspective,
+being reused for versatile situations.
+
+## Section 1
+
+Your content here.
+
+## Conclusion
+
+Final thoughts.
---
+++ new_post.sh
@@ -0,0 +1,35 @@
+# 입력된 제목을 포스트 파일 이름으로 변환
+title=$1
+filename=$(date +%Y-%m-%d)-$(echo "$title" | sed 's/ /-/g').md
+
+# _posts 디렉토리로 이동
+cd _posts
+
+# 템플릿 파일 생성
+cat <<EOT >> $filename
+---
+layout: post
+title: "$title"
+date: $(date +%Y-%m-%d' '%H:%M:%S' '%z)
+categories: [category1, category2]
+tags: [tag1, tag2]
+toc: true
+toc_label: "Table of Contents"
+toc_icon: "list"
+author: "Your Name"
+---
+
+## Introduction
+
+Write your introduction here.
+
+## Section 1
+
+Your content here.
+
+## Conclusion
+
+Final thoughts.
+EOT
+
+echo "Post $filename created."
